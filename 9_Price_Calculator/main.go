985826027f04529f80125958538fc0d8324ed5a8
Jimmy Yen <ichthyoid95@gmail.com>
2024-08-06 10:59:29 +0800

    [Section #9] Practice Project: Price Calculator - using Interface

--- 9_Price_Calculator/main.go
+++ 9_Price_Calculator/main.go
@@ -1,17 +1,17 @@
 package main
 
 import (
-	"example.com/price-calculator/filemanager"
+	"example.com/price-calculator/cmdmanager"
 	"example.com/price-calculator/prices"
-	"fmt"
 )
 
 func main() {
 	taxRates := []float64{0, 0.07, 0.10, 0.15}
 
 	for _, taxRate := range taxRates {
-		fm := filemanager.New("prices.txt", fmt.Sprintf("result_%.0f.json", taxRate*100))
-		priceJob := prices.NewTaxIncludedPriceJob(fm, taxRate)
+		// fm := filemanager.New("prices.txt", fmt.Sprintf("result_%.0f.json", taxRate*100))
+		cmdm := cmdmanager.New()
+		priceJob := prices.NewTaxIncludedPriceJob(cmdm, taxRate)
 		priceJob.Process()
 	}
 }
