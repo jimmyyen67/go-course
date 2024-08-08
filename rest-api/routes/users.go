f672e52522cb953b21665270ba8f945642ad4071
Jimmy Yen <ichthyoid95@gmail.com>
2024-08-08 18:07:24 +0800

    Course Project: RESTful API - password hashed in database - login and check hashed password

---
+++ rest-api/routes/users.go
@@ -0,0 +1,42 @@
+package routes
+
+import (
+	"net/http"
+
+	"example.com/rest-api/models"
+	"github.com/gin-gonic/gin"
+)
+
+func signup(context *gin.Context) {
+	var user models.User
+	err := context.ShouldBindJSON(&user)
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"message": "could not parse request data"})
+		return
+	}
+
+	err = user.Save()
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"message": "could not save user into database"})
+		return
+	}
+
+	context.JSON(http.StatusCreated, gin.H{"message": "user created successfully"})
+}
+
+func login(context *gin.Context) {
+	var user models.User
+	err := context.ShouldBindJSON(&user)
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"message": "could not parse request data of logged in"})
+		return
+	}
+
+	err = user.ValidateCredentials()
+	if err != nil {
+		context.JSON(http.StatusUnauthorized, gin.H{"message": "could not authenticate user"})
+		return
+	}
+
+	context.JSON(http.StatusOK, gin.H{"message": "user logged in"})
+}
